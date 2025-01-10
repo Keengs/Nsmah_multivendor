cafd404120b9d3173c10237ad565a803db6cf4ea
Plannet <plannet247@gmail.com>
2025-01-10 15:33:55 +0400

    basic setup, shadcn , theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <div className="w-100 flex justify-end">
+        <ThemeToggle />
+      </div>
      <h1 className="fon-bold text-blue-500">Welcome to Nsmah!</h1>
+     <Button>Click Here</Button>
     </div>
   );
 }
